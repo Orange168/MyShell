e718aa85be14c403f2381c22a1e3c90b5b017f98
Austin L.Gable <zhilinchn@gmail.com>
2016-01-08 22:15:31 +0800

    chapter 37

---
+++ chapter2/forfile.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+for file in /{,usr/}bin/*ep
+#             ^    Find all executable files ending in "calc"
+#+                 in /bin and /usr/bin directories.
+do
+        if [ -x "$file" ]
+        then
+          echo $file
+        fi
+done
